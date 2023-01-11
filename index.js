9c6be4bba41dfa33ededb1aa78ee66195bdf0272
zsorosz <oroszi.zsofi@gmail.com>
2023-01-11 11:58:21 +0100

    Iteration-1 done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Iteration 1: Names and Input
+const hacker1 = "Zsofia";
+console.log(`The driver's name is ${hacker1}.`);
 
+const hacker2 = "Istvan";
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
